2687c3c99bac1d76887587170e2cb16d05c541bf
Tom Makin <tom.makin@gmail.com>
2018-08-12 18:24:45 +0100

    restored live upload config

--- scripts/live_upload.sh
+++ scripts/live_upload.sh
@@ -4,8 +4,6 @@ source /opt/env.sh
 
 path=/recordings/$1
 
-echo $path &> /tmp/test_upload.log
-
 yamdi -i $path -o /videos/$1
 
-python3 /opt/upload.py $path $TEST_STORAGE_ACCOUNT $TEST_SAS_KEY &> /tmp/test_upload.log
+python3 /opt/upload.py $path $STORAGE_ACCOUNT $SAS_KEY >> /tmp/upload.log
